ddf7a73354acce2aa717a9c1b6139a2fbd7f11a3
MaahirHussainShaik <142274442+MaahirHussainShaik@users.noreply.github.com>
2024-06-24 18:06:16 +1200

    the url

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 'use client'
 
 export default function myImageLoader({ src, width, quality }) {
-    return `https://maahirsportfolio.vercel.app/${src}?w=${width}&q=${quality || 75}`;
+    return `https://maahirsportfolio-world.vercel.app/${src}?w=${width}&q=${quality || 75}`;
 }
