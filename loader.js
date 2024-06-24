8d330c03a09889388bcc00879d69208eeab11d65
MaahirHussainShaik <142274442+MaahirHussainShaik@users.noreply.github.com>
2024-06-24 18:14:29 +1200

    vercel deploy

--- loader.js
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function myImageLoader({ src, width, quality }) {
-    return `https://maahirsportfolio-world.vercel.app/${src}?w=${width}&q=${quality || 75}`;
-}
--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Custom loader for .svg and .png files
-    config.module.rules.push({
-      test: /\.(svg|png)$/,
-      use: [
-        {
-          loader: 'custom-loader',
-          options: {
-            loaderFile: './loader.js'
-          }
-        }
-      ]
-    });
-
-    return config;
-  }
 };
 
 module.exports = nextConfig;
