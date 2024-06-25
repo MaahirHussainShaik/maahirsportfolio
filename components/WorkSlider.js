bc8d3c620b2064396271c34c4125b83c234d6932
MaahirHussainShaik <142274442+MaahirHussainShaik@users.noreply.github.com>
2024-06-25 18:29:52 +1200

    added arrows and removed the draggable images

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,8 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper';
-import { BsArrowRight } from 'react-icons/bs';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper';
+import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
 import Image from "next/image";
 
 const workSlider = {
@@ -36,30 +37,38 @@ const workSlider = {
 
 const WorkSlider = ({ setActiveSlide }) => {
   return (
-    <Swiper
-      spaceBetween={10}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
-      className="h-[280px] sm:h-[480px]"
-    >
-      {workSlider.slides.map((slide, index) => (
-        <SwiperSlide key={index}>
-          <a href={slide.url} target="_blank" rel="noopener noreferrer" className='relative rounded-lg overflow-hidden flex flex-col items-center justify-center group'>
-            <div className='relative overflow-hidden'>
-              <Image src={slide.path} width={500} height={300} alt={slide.title || 'Image description'} />
-              <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
-            </div>
-            <div className='mt-4 text-center'>
-              <h3 className='text-xl font-bold'>{slide.title}</h3>
-              <p className='mt-2'>{slide.description}</p>
-            </div>
-          </a>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative">
+      <Swiper
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        modules={[Pagination, Navigation]}
+        onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+        className="h-[280px] sm:h-[480px]"
+      >
+        {workSlider.slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <a href={slide.url} target="_blank" rel="noopener noreferrer" className='relative rounded-lg overflow-hidden flex flex-col items-center justify-center group'>
+              <div className='relative overflow-hidden'>
+                <Image src={slide.path} width={500} height={300} alt={slide.title || 'Image description'} draggable="false" />
+                <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+              </div>
+              <div className='mt-4 text-center'>
+                <h3 className='text-xl font-bold'>{slide.title}</h3>
+                <p className='mt-2'>{slide.description}</p>
+              </div>
+            </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swiper-button-next"><BsArrowRight /></div>
+      <div className="swiper-button-prev"><BsArrowLeft /></div>
+    </div>
   );
 };
 
